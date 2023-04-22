e5e43a6e0d8ecb62c206c8bc9888646060af2698
Akash Pandey <akashp519519@gmail.com>
2023-04-22 15:05:26 +0530

    Api Creation using Django

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from api.models import Company
+# Create Serializers here
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    company_id=serializers.ReadOnlyField()
+    class Meta:
+        model=Company
+        fields="__all__"
