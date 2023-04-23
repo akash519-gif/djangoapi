7a97b2d60ed1858fcab95f311d59374d527d219d
Akash Pandey <akashp519519@gmail.com>
2023-04-23 13:22:11 +0530

    Disabled browsable api and added superuser.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from api.models import Company, Employee
 
 # Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display=('name','location','type')
+    search_fields=('name',) 
+    # Here above it needs tuple or list that's why we give one value and put "," after it showing that next value is null.
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('name','email','company')
+    list_filter=('company',)
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Employee, EmployeeAdmin)
